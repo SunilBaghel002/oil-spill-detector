e10bbd51d4e2ee0722fbb19319f786bf1b5f872e
SunilBaghel002 <sunilbaghel93100@gmail.com>
2024-09-09 22:27:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+
+window.onload = function () {
+    setTimeout(function () {
+        alert("Oil spill detected in ship4. information of ship 4 are sent to Bangladesh.")
+
+    }, 14000);
+
+    setTimeout(function () {
+        alert("Oil spill detected in ship7. information of ship 7 are sent to Thailand.")
+    }, 29000);
+
+    setTimeout(function () {
+        alert("Oil spill detected in ship6. information of ship 6 are sent to Sri Lanka.")
+    }, 44000);
+
+    setTimeout(function () {
+        alert("Oil spill detected in ship2. information of ship 2 are sent to India.")
+    }, 59000);
+
+    setTimeout(function () {
+        alert("Oil spill detected in ship1. information of ship 1 are sent to India.")
+    }, 74000);
+
+    setTimeout(function () {
+        alert("Oil spill detected in ship3. information of ship 3 are sent to UAE.")
+    }, 89000);
+
+    setTimeout(function () {
+        alert("Oil spill detected in ship5. information of ship 5 are sent to Nearest country!!!!....")
+    }, 104000);
+    setTimeout(function () {
+        document.querySelector(".area4").style.backgroundColor = "white"
+    }, 13500);
+    setTimeout(function () {
+        document.querySelector(".area7").style.backgroundColor = "white"
+    }, 28500);
+    setTimeout(function () {
+        document.querySelector(".area6").style.backgroundColor = "white"
+    }, 43500);
+    setTimeout(function () {
+        document.querySelector(".area2").style.backgroundColor = "white"
+    }, 58500);
+    setTimeout(function () {
+        document.querySelector(".area1").style.backgroundColor = "white"
+    }, 73500);
+    setTimeout(function () {
+        document.querySelector(".area3").style.backgroundColor = "white"
+    }, 88500);
+    setTimeout(function () {
+        document.querySelector(".area5").style.backgroundColor = "white"
+    }, 103500);
+};
